d62b5dc2215204145a4687333c2c5bd5c98cee39
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2021-10-05 21:47:42 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -31,7 +31,7 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>╭───「  ⭕️ BOT STATISTICS ⭕️ 」 </b>\n' \
+    stats = f'<b>╭───「  ⭕️ BOT STATS ⭕️ 」 </b>\n' \
             f'<b>│</b>\n' \
             f'<b>├  ⏰ Bot Uptime:</b> <code>{currentTime}</code>\n' \
             f'<b>├  🗄 Total Disk Space:</b> <code>{total}</code>\n' \
